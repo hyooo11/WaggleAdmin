7d89ff2990fc5cfe57f7aa2ffe9606644b91b113
hyooo11 <tlsgywls1107@naver.com>
2024-02-29 16:32:24 +0900

    style: login 페이지 스타일링

--- src/component/auth/Login.tsx
+++ src/component/auth/Login.tsx
@@ -32,12 +32,14 @@ const Login = () => {
   return (
     <div className="Login">
       <div className="input_box">
+        <p className="desc">관리자 계정으로 로그인 해주세요 !</p>
         <div>
           <input
             type="text"
             onChange={(e) => {
               setId(e.target.value);
             }}
+            placeholder="아이디 입력"
             className="form-control"
           />
         </div>
@@ -47,13 +49,14 @@ const Login = () => {
             onChange={(e) => {
               setPassword(e.target.value);
             }}
+            placeholder="비밀번호 입력"
             className="form-control"
           />
         </div>
+        <button type="submit" onClick={onSubmit} className="btn_login">
+          로그인
+        </button>
       </div>
-      <button type="submit" onClick={onSubmit}>
-        로그인
-      </button>
     </div>
   );
 };
